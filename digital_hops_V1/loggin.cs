2e18a96dbf142acfbcb7cbfb9c14732b04b38ffa
sandr <sandr@DESKTOP-J51FLS5>
2022-07-02 08:10:47 -0500

    Agregar archivos de proyecto.

---
+++ digital_hops_V1/loggin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace digital_hops_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Click(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "Hola a todos!";
+        }
+
+        private void btnDespedida_Click(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "Adios!";
+            this.Text = "Bye!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "";
+        }
+    }
+}
